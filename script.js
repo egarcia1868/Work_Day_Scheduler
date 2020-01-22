a737314514dabad4d751b315bc1887e14ff95a7a
Eric Garcia <egarcia1868@gmail.com>
2020-01-22 16:44:24 -0600

    getting my current date to show up properly

--- script.js
+++ script.js
@@ -1,23 +1,3 @@
-
-
-
-
-
-function storeSchedule() {
-  // Stringify and set "highScores" key in localStorage to highScores array
-  localStorage.setItem("highScores", JSON.stringify(highScores));
-}
-
-function renderSchedule() {
-  // Clear highScoresList element and update highScoresCount
-  highScoresListEl.innerHTML = "";
-}
-
-// When the clear highscores button is clicked...
-document.querySelector("#clearScores").addEventListener("click", function(event) {
-  highScores = [];
-
-  //   // Store empty highScores in localStorage, re-render the empty list
-    storeHighScores();
-    renderHighScores();
-  })
+$(document).ready(function() {
+  $("#currentDate").append(moment().format("dddd, MMMM Do"));
+})
