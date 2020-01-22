e105358e0dfc8b7ef43efa3cc4200c84faadac0b
Eric Garcia <egarcia1868@gmail.com>
2020-01-22 17:37:29 -0600

    about to break everything.  Current setup is making targeting weird.  pretty sure js is working right.

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
 $(document).ready(function() {
   $("#currentDate").append(moment().format("dddd, MMMM Do"));
+
+  var nineVal = localStorage.getItem("task9", nineVal);
+      $("#task9").val(nineVal);
+
+  $("#save9").click(function() {
+    nineVal = $("#task9").val();
+    localStorage.setItem("task9", nineVal);
+  });
+  $("#save10").click();
+  $("#save11").click();
+  $("#save12").click();
+  $("#save1").click();
+  $("#save2").click();
+  $("#save3").click();
+  $("#save4").click();
+  $("#save5").click();
+
 })
