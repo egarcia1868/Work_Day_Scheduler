feb0ec3605e4cfbf9c08b83f41588e23ed0a6a59
Eric Garcia <egarcia1868@gmail.com>
2020-01-23 14:19:59 -0600

    correcting my currenthour/hour comparator and adding the floppy disk icon.

--- script.js
+++ script.js
@@ -2,9 +2,10 @@ var currentHour = moment().format("H");
 console.log(currentHour)
 
 for (var hour = 9; hour < 18; hour++) {
-  if (currentHour === hour) {
-    document.querySelector(`#task${currentHour}`).classList.remove("future");
-    document.querySelector(`#task${currentHour}`).classList.add("present");
+  console.log(hour);
+  if (currentHour == hour) {
+    document.querySelector(`#task${hour}`).classList.remove("future");
+    document.querySelector(`#task${hour}`).classList.add("present");
   }
   if (currentHour > hour) {
     document.querySelector(`#task${hour}`).classList.remove("future");
